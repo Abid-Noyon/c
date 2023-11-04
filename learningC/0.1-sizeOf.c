a408903fee53b80e818b0797d2385668b8de7cd6
Abid-Noyon <abidnoyon49@gmail.com>
2023-11-04 16:55:52 +0600

    updated files

---
+++ learningC/0.1-sizeOf.c
@@ -0,0 +1,13 @@
+// C Program To demonstrate
+// sizeof operator
+#include <stdio.h>
+int main()
+{
+    printf("%zu\n", sizeof(char));
+    printf("%zu\n", sizeof(int));
+    printf("%zu\n", sizeof(float));
+    printf("%zu", sizeof(double));
+    printf("");
+    printf("");
+    return 0;
+}
---
+++ learningC/1-test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    char yourName[30];
+    scanf("%d", yourName);
+    printf("\nWelcome: %c", yourName);
+    return 0;
+}
