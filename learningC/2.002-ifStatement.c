dcedd92befc2148bee03976ff8f38c8e119287ec
Abid-Noyon <abidnoyon49@gmail.com>
2024-02-18 07:48:03 +0600

    updated files

---
+++ learningC/2.002-ifStatement.c
@@ -0,0 +1,24 @@
+// this program take a number and convert it feet/meter on users choice
+
+#include <stdio.h>
+
+int main(void)
+{
+    float num;
+    int choice;
+    printf("Enter a number to convert: ");
+    scanf("%f", &num);
+    printf("1: Feet to meter.\n");
+    printf("2: Meter to feet.\n");
+    printf("Enter choice: ");
+    scanf("%d", &choice);
+    if (choice == 1)
+    {
+        printf("%f m.", num / 3.28);
+    }
+    if (choice == 2)
+    {
+        printf("%f f.", num * 3.28);
+    }
+    return 0;
+}
