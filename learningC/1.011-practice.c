020f5cf424574155fcf7b51e0b1cfb10c108cf11
Abid-Noyon <abidnoyon49@gmail.com>
2024-02-20 09:53:47 +0600

    updated files

---
+++ learningC/1.011-practice.c
@@ -0,0 +1,51 @@
+/*The printf( ) function is used to output information to the
+screen. Write a program that displays This is the number 100.
+(Output the 100 as a number, not as a string.)*/
+
+// #include <stdio.h>
+
+// int outnum(int x);
+
+// int main()
+// {
+//     printf("This is the number %d.", outnum(100));
+//     return 0;
+// }
+
+// int outnum(int x)
+// {
+//     // printf("%d", x);
+//     return x;
+// }
+
+// or just simply
+// #include <stdio.h>
+
+// int main()
+// {
+//     // Displaying the number 100 using printf()
+//     printf("This is the number %d.\n", 100);
+
+//     return 0;
+// }
+
+/*problem 3: 7. Write a program that inputs an integer from the keyboard and
+displays its square.*/
+// #include <stdio.h>
+
+// int sqrIt(void);
+
+// int main(void)
+// {
+//     // sqrIt();
+//     printf("The square of the number: %d", sqrIt());
+//     return 0;
+// }
+
+// int sqrIt(void)
+// {
+//     int num;
+//     printf("Enter a number to square it: ");
+//     scanf("%d", &num);
+//     return num * num;
+// }
